704355e9a2079ffe17b268a3fbb0a2216a643298
Sergio del Amo <sergio.delamo@softamo.com>
2021-10-14 14:27:26 +0200

    Update travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -3,12 +3,12 @@ set -e
 
 export EXIT_STATUS=0
 
-curl -O https://raw.githubusercontent.com/grails/grails-guides/master/travis/build-guide
+curl -O https://raw.githubusercontent.com/grails/grails-guides/master/githubactions/build-guide
 chmod 777 build-guide
 
 ./build-guide || EXIT_STATUS=$?
 
-curl -O https://raw.githubusercontent.com/grails/grails-guides/master/travis/republish-guides-website.sh
+curl -O https://raw.githubusercontent.com/grails/grails-guides/master/githubactions/republish-guides-website.sh
 chmod 777 republish-guides-website.sh
 
 ./republish-guides-website.sh || EXIT_STATUS=$?
