3719e4a9c698bc569098a3d8d60be7f1e034de87
Sergio del Amo <sergio.delamo@softamo.com>
2021-10-14 16:13:47 +0200

    Update travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -8,6 +8,11 @@ chmod 777 build-guide
 
 ./build-guide || EXIT_STATUS=$?
 
+if [[ $EXIT_STATUS -ne 0 ]]; then
+    echo "build-guide failed" 
+    exit $EXIT_STATUS
+fi  
+
 curl -O https://raw.githubusercontent.com/grails/grails-guides/master/githubactions/republish-guides-website.sh
 chmod 777 republish-guides-website.sh
 
